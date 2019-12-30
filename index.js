ffce4991a18736fa9f88581575d05f07a1fbbec9
LAPTOP-IEJHC722\ki59920 <kuomingwu@gmail.com>
2019-12-30 13:35:54 +0800

    express

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'path';
+import bodyParser  from 'body-parser';
+import helmet from 'helmet';
+import { serverPort , debugMode } from './serverConfig';
+require('express-group-routes');
+const session = require('express-session');
+const app = express();
+app.use(express.static('src'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+if(debugMode == true){
+	app.use(cors());
+}else{
+	
+	app.use(helmet());
+}
+app.set('trust proxy', 1) // trust first proxy
+
+
+
+app.group('/hello' , (router)=>{
+	
+	router.get('/world' , async(req , res , next)=>{
+		
+		res.send("helloworld !") ; 
+		
+	})
+	
+	
+})
+
+app.group('/' , (router)=>{
+	
+	router.get('*' , (req , res , next)=>{
+		
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+		
+	})
+	
+})
+
+app.listen(serverPort, function () {
+	console.log(`Example app listening on port ${serverPort}!`);
+});
---
+++ serverConfig.js
@@ -0,0 +1,2 @@
+export const serverPort = 80 ; 
+export const debugMode = true ; 
